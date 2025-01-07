a13fd4678ffc97900ee356ca9d7ec30f9c0c5c5b
CedricA <cedric.j.aoun@gmail.com>
2025-01-07 15:32:07 +0100

    added basic drag and drop functionality

--- script.js
+++ script.js
@@ -6,6 +6,8 @@ window.addEventListener("DOMContentLoaded", () => {
   const addCardBtn = document.getElementById('addCardBtn');
   const searchInput = document.getElementById('searchInput');
   const sortByPriorityBtn = document.getElementById('sortByPriorityBtn');
+  const allCards = document.querySelectorAll('.card');
+  const dropZones = document.querySelectorAll('.column');
 
   // Éventuellement, on écoute les événements
   addCardBtn.addEventListener('click', () => {
@@ -19,4 +21,39 @@ window.addEventListener("DOMContentLoaded", () => {
   sortByPriorityBtn.addEventListener('click', () => {
     // ...
   });
+
+
+  // DRAG AND DROP FEATURE
+  // CARDS
+  for (let card of allCards) {
+    card.addEventListener('dragstart', (e) => {
+      e.dataTransfer.setData('text/plain', card.dataset.id);
+      card.style.opacity = '0.5';
+    });
+
+    card.addEventListener('dragend', () => {
+      card.style.opacity = '1';
+    });
+  }
+  // DROPZONES
+  for (let zone of dropZones) {
+    zone.addEventListener('dragover', (e) => {
+      e.preventDefault();
+      zone.classList.add('hover');
+    });
+
+    zone.addEventListener('dragleave', () => {
+      zone.classList.remove('hover');
+    });
+
+    zone.addEventListener('drop', (e) => {
+      e.preventDefault();
+      zone.classList.remove('hover');
+      const dataId = e.dataTransfer.getData('text/plain');
+      const draggedElement = document.querySelector(`[data-id="${dataId}"]`);
+      zone.appendChild(draggedElement);
+    });
+  }
+
+
 });
